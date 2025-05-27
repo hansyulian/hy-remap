2c1b4150196a70285d1b7810519fb39ea76b691e
Hans Yulian <10959546+hansyulian@users.noreply.github.com>
2025-05-27 18:41:51 +0700

    mouse click can hang if we have a certain profile shift action with sequence: mouse down -> profile shift down -> mouse up -> profile shift up. the mouse will stay in down state (#7)

--- src/controllers/input_remapping.cpp
+++ src/controllers/input_remapping.cpp
@@ -1,26 +1,32 @@
 #include "controllers.h"
 #include <chrono>
 
-bool processInputRemapping(const KeyAction& inputTrigger){
-    if (inputTrigger.up){
-        releaseOngoingAction(inputTrigger.keyCode, false);     
-    }   
+bool processInputRemapping(const KeyAction &inputTrigger)
+{
+    bool released = true;
+    if (inputTrigger.up)
+    {
+        released = releaseOngoingAction(inputTrigger.keyCode, false);
+    }
     auto keyCode = inputTrigger.keyCode;
     auto up = inputTrigger.up;
     auto profileIndex = getActiveProfileIndex(); // profile is guaranteed to exist, at least the default profile
-    if (profileIndex <= NO_PROFILE_FLAG){
+    if (profileIndex <= NO_PROFILE_FLAG)
+    {
         return false;
     }
-    
+
     auto actionIndex = optimizedProfiles[profileIndex].actionIdMap[keyCode];
-    if (actionIndex == TERMINATE_ACTION_FLAG){
+    if (actionIndex == TERMINATE_ACTION_FLAG)
+    {
         return true;
     }
-    if (actionIndex <= NO_ACTION_FLAG){
+    if (actionIndex <= NO_ACTION_FLAG)
+    {
         return false;
     }
     const OptimizedAction action = optimizedActions[actionIndex];
-    
+
     performAction(action, inputTrigger);
-    return true;
+    return released;
 }
--- src/controllers/release_action.cpp
+++ src/controllers/release_action.cpp
@@ -1,11 +1,13 @@
 #include "controllers.h"
 #include "../actions/actions.h"
 
-void releaseOngoingAction(int keyCode, bool force){
+bool releaseOngoingAction(int keyCode, bool force)
+{
   // cout << "Release action force" << force << endl;
   auto actionIndex = keyDownActionIndex[keyCode];
-  if (actionIndex < 0){
-    return;
+  if (actionIndex < 0)
+  {
+    return false;
   }
   // cout << "Action index " << keyCode <<" | keyCode" <<actionIndex << endl;
   auto action = &optimizedActions[actionIndex];
@@ -13,19 +15,20 @@ void releaseOngoingAction(int keyCode, bool force){
   keyDownActionIndex[keyCode] = NO_ACTION_FLAG;
   // cout << "releasing " << keyCode << " " <<keyDownActionIndex[keyCode] << endl;
   // cout << "Releasing action " << actionIndex <<  " " << action->name << " " << force << endl;
-  switch(action->type){
-    case ActionType::SIMPLE:
-      releaseSimpleAction(*action);
-      return;
-    case ActionType::PROFILE_SHIFT:
-      // cout << "Releasing profile shift" << endl;
-      releaseProfileShiftAction(*action, keyCode);
-      return;
-    case ActionType::MACRO:
-      // cout << "Releasing macro" << endl;
-      releaseMacroAction(*action, force);
-      return;
-    default:
-      return;
+  switch (action->type)
+  {
+  case ActionType::SIMPLE:
+    releaseSimpleAction(*action);
+    return true;
+  case ActionType::PROFILE_SHIFT:
+    // cout << "Releasing profile shift" << endl;
+    releaseProfileShiftAction(*action, keyCode);
+    return true;
+  case ActionType::MACRO:
+    // cout << "Releasing macro" << endl;
+    releaseMacroAction(*action, force);
+    return true;
+  default:
+    return true;
   }
 }
